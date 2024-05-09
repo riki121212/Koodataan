7b8ffc648ea4897b576e14697111fabe9a65e630
riki121212 <rleppa123@gmail.com>
2024-05-09 12:49:11 +0300

    Add files via upload

---
+++ Sääohjelma koodi.py
@@ -0,0 +1,27 @@
+import requests
+
+def hae_saa(kaupunki):
+    api_avain = "a5f258ebce62bab18e7ea5967a2ea1f3"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={kaupunki}&appid={api_avain}&units=metric"
+    vastaus = requests.get(url)
+    data = vastaus.json()
+    return data
+
+def main():
+    kaupunki = input("Syötä kaupungin nimi: ")
+    sää = hae_saa(kaupunki)
+
+    if sää["cod"] == 200:
+        lämpötila = sää["main"]["temp"]
+        print(f"Sää kaupungissa {kaupunki}: {lämpötila}°C")
+
+        if lämpötila >= 10:
+            print("Kannattaa matkustaa!")
+        else:
+            print("Ehkä kannattaa odottaa vähän parempaa säätä.")
+
+    else:
+        print("Kaupunkia ei löytynyt. Yritä uudestaan.")
+
+if __name__ == "__main__":
+    main()
